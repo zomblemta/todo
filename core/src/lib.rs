5f2be08b80b000a7bb870ce43500fd8a42f414af
Alen.Ma <alen.ma@youniverse.cc>
2023-08-22 18:31:54 +0800

    添加上prisma

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,5 +1,17 @@
+use prisma_client_rust::PrismaClient;
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
+}
+
+pub struct Node {
+    db: Arc<PrismaClient>
+}
+
+impl  Node {
+    pub fn new(self) {
+        db
+    }
 }
 
 #[cfg(test)]
